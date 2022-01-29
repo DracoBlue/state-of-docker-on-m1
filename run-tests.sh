7c6c6759554aa5a04490188e9b234a372c96b770
DracoBlue <jans@dracoblue.de>
2022-01-29 14:28:14 +0100

    Added lsb_release and sw_vers output

--- run-tests.sh
+++ run-tests.sh
@@ -10,7 +10,7 @@ fi
 
 if [ `which limactl` ]
 then
-  echo "--Limactl-Version----------------------------------------------------"
+  echo "--Limactl-Version-------------------------------------------------"
   limactl --version
   echo "--Limactl-Info----------------------------------------------------"
   limactl info
@@ -21,14 +21,26 @@ then
   echo "--Docker----------------------------------------------------------"
   docker version
 
-  echo "--binfmt------------------------------------------------------------"
+  echo "--binfmt----------------------------------------------------------"
   docker run --privileged --rm tonistiigi/binfmt
 fi
 
 if [ `which docker-compose` ]
 then
-  echo "--Docker-Compose---------------------------------------------------"
+  echo "--Docker-Compose--------------------------------------------------"
   docker-compose version
+fi
+
+if [ `which lsb_release` ]
+then
+  echo "--Lsb-Release-----------------------------------------------------"
+  lsb_release -a
+fi
+
+if [ `which sw_vers` ]
+then
+  echo "--Sw-Vers---------------------------------------------------------"
+  sw_vers
 fi
 
 echo "--------------------------------------------------------------------"
